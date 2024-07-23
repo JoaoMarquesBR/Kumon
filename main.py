6a813d6c308a547805498b39c0f9a33e2e9badab
Joao Marques <johnmarques.br2002@gmail.com>
2024-07-23 07:48:11 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,52 @@
+import random 
+
+def header():
+    print("Welcome to Math Practice 1.0.0\n")
+
+def question_return(question):
+    return (int)(input(question))
+
+def highest_digits(digits):
+    print("hello")
+
+def start_multiplication(lowest_a_number,highest_a_number,lowest_b_number,highest_b_number):
+    print("\nStarting multiplication exercises")
+    while True:
+        random_number_a = random.randint(lowest_a_number,highest_a_number)
+        random_number_b = random.randint(lowest_b_number,highest_b_number)
+        response = int(input(f"{random_number_a} * {random_number_b}: "))
+        
+        if ( random_number_a * random_number_b) == response : 
+            print("Correct resposne")
+        else:
+            print("Wrong response")
+        print("")
+
+def multiplication_setup():
+    two_digits = False
+    print("Numbers = A * B")
+    
+    print("\n")
+    lowest_a_number = (int)(input("lowest number for A:"))
+    highest_a_number = (int)(input("highest number for A:"))
+
+    print("\n")
+    lowest_b_number = (int)(input("lowest number for B:"))
+    highest_b_number = (int)(input("highest number for B:"))
+
+    start_multiplication(lowest_a_number,highest_a_number,lowest_b_number,highest_b_number)
+
+def select_math_practice(value):
+    print("\n")
+    if value == 1:
+        multiplication_setup()
+
+def menu_selection():
+    print("1 - Practice Multiplication")
+    print("\n")
+    option_selected = (int)(input("Input Option: "))
+    select_math_practice(option_selected)
+
+
+header()
+menu_selection()
