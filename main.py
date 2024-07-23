3c91c47071b9950d7eb7df13b7bc40c19187be10
Joao Marques <johnmarques.br2002@gmail.com>
2024-07-23 08:02:26 -0300

    Colours for output

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import random 
+RESET = "\033[0m"
+RED = "\033[31m"
+GREEN = "\033[32m"
+YELLOW = "\033[33m"
+BLUE = "\033[34m"
 
 def header():
     print("Welcome to Math Practice 1.0.0\n")
@@ -10,27 +15,28 @@ def highest_digits(digits):
     print("hello")
 
 def start_multiplication(lowest_a_number,highest_a_number,lowest_b_number,highest_b_number):
-    print("\nStarting multiplication exercises")
+    print("\nStarting multiplication exercises\n")
     while True:
         random_number_a = random.randint(lowest_a_number,highest_a_number)
         random_number_b = random.randint(lowest_b_number,highest_b_number)
-        response = int(input(f"{random_number_a} * {random_number_b}: "))
+
+        while True : 
+            response = int(input(f"{random_number_a} * {random_number_b}: "))
         
-        if ( random_number_a * random_number_b) == response : 
-            print("Correct resposne")
-        else:
-            print("Wrong response")
-        print("")
+            if ( random_number_a * random_number_b) == response : 
+                print(f"{GREEN}Correct response{RESET}\n")
+                break
+            else:
+                print(f"{RED}Wrong response, please try again\n{RESET}")
+            
 
 def multiplication_setup():
     two_digits = False
-    print("Numbers = A * B")
-    
-    print("\n")
+    print("Numbers = A * B \n")
     lowest_a_number = (int)(input("lowest number for A:"))
     highest_a_number = (int)(input("highest number for A:"))
 
-    print("\n")
+    print("")
     lowest_b_number = (int)(input("lowest number for B:"))
     highest_b_number = (int)(input("highest number for B:"))
 
@@ -42,8 +48,7 @@ def select_math_practice(value):
         multiplication_setup()
 
 def menu_selection():
-    print("1 - Practice Multiplication")
-    print("\n")
+    print("1 - Practice Multiplication\n")
     option_selected = (int)(input("Input Option: "))
     select_math_practice(option_selected)
 
